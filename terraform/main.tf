7f44100d6f6f072cb6ccac37ba41478e1acb7a05
LeoPincle <leo.soto965@gmail.com>
2025-10-29 13:19:36 -0600

    editing tf files

---
+++ terraform/main.tf
@@ -0,0 +1,43 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "${var.aks_name}-dns"
+
+  default_node_pool {
+    name       = "system"
+    node_count = 2
+    vm_size    = "Standard_B4ms"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin = "azure"
+    load_balancer_sku = "standard"
+  }
+
+  role_based_access_control_enabled = true
+}
+
+# Allow AKS to pull from ACR
+resource "azurerm_role_assignment" "aks_acr_pull" {
+  scope                = azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,4 @@
+variable "resource_group_name" { type = string }
+variable "location"            { type = string  default = "eastus" }
+variable "acr_name"            { type = string }
+variable "aks_name"            { type = string }
